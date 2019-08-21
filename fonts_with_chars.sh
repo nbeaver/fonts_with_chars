af8ff5fa7131efb7ae6b1d902cab46a9838a2f31
Nathaniel Morck Beaver <nathanielmbeaver@gmail.com>
2019-08-21 11:55:55 -0400

    Be more picky about arguments.

--- fonts_with_chars.sh
+++ fonts_with_chars.sh
@@ -1,4 +1,21 @@
 #! /usr/bin/env bash
+usage() {
+    >&2 printf "Usage: %s character\n" "$0"
+}
+if test $# -eq 0
+then
+    usage
+    exit 1
+elif test $# -ne 1
+then
+    >&2 printf 'Error: single characters only.\n'
+    usage
+    exit 1
+elif test $* = "-h"
+then
+    usage
+    exit 1
+fi
 char=$*
 hex_codepoint=$(printf '%x' \'"${char}")
 fc-list ":charset=${hex_codepoint}"
